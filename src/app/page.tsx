47ff042933a65fcad25abd47541d64381dbff4a3
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-07-12 04:15:07 +0200

    fixed deployment bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import SecondSection from "components/SecondSection";
 import ThirdSection from "components/thirdSection";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
